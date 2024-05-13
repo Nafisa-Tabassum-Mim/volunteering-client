52e5b21ccabbf2d1d8bfeedcd958f40f2bfed4b7
Nafisa-Tabassum-Mim <mim.afrin129@gmail.com>
2024-05-14 04:32:57 +0600

    form added

--- src/components/ManageMyPost/MyVolunteerRequest.jsx
+++ src/components/ManageMyPost/MyVolunteerRequest.jsx
@@ -1,10 +1,95 @@
-import React from 'react';
+import  { useEffect, useState } from 'react';
 
 const MyVolunteerRequest = () => {
+    const [posts, setPosts] = useState([])
+
+    
+    useEffect(() => {
+        fetch('http://localhost:5000/request')
+            .then(res => res.json())
+            .then((data) => setPosts(data))
+    }, [])
+
+    const handleDelete = (_id) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/post/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your vacancy volunteer post has been deleted.',
+                                'success'
+                            )
+                            const remaining = posts.filter(post => post._id !== _id);
+                            setPosts(remaining);
+                        }
+                    })
+
+            }
+        })
+    };
+    
     return (
         <div>
-            <h3>My Volunteering Request</h3>
+        <div className="overflow-x-auto">
+            {
+                // posts.length !== 0 ?
+                posts ?
+                    (
+                        <>
+                            <table className="table">
+                                <caption className="text-lg md:text-2xl text-center font-semibold my-4 font-mono">My Volunteer request post</caption>
+
+                                {/* head */}
+                                <thead>
+                                    <tr className='text-medium md:text-lg font-mono'>
+                                        <th>Post Title</th>
+                                        <th>Category</th>
+                                        <th>Location</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {posts.map((post) => (
+                                        <tr key={post._id} className='text-medium md:text-lg font-mono'>
+                                            <td>{post.post_title}</td>
+                                            <td>{post.category}</td>
+                                            <td>{post.location}</td>
+                                            <td className="px-2 py-4">
+                          
+
+                                            <button onClick={() => handleDelete(post._id)} className="text-green-500 border-green-500 border-2 p-2 rounded-xl hover:bg-red-500">Cancel</button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </>)
+                        :
+                        (<div className=" h-[400px] w-full flex flex-col justify-center items-center gap-2">
+                            <p className="text-2xl font-semibold">Your post Data is empty</p>
+                            <p className="text-2xl font-semibold">Add your data <span className="text-green-500">now !</span> </p>
+                            <Link className="btn text-white bg-green-500 ">Add Now !</Link>
+                        </div>)
+
+                }
+            </div>
         </div>
+
     );
 };
 
--- src/components/ManageMyPost/VolunteerVacancy.jsx
+++ src/components/ManageMyPost/VolunteerVacancy.jsx
@@ -2,10 +2,17 @@ import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../firebase/AuthProvider';
 import { Link } from 'react-router-dom';
 import Swal from "sweetalert2";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify"
+
 
 const VolunteerVacancy = () => {
     const { user } = useContext(AuthContext)
     const [posts, setPosts] = useState([])
+    const [startDate, setStartDate] = useState(new Date());
+
 
 
     useEffect(() => {
@@ -48,6 +55,40 @@ const VolunteerVacancy = () => {
     };
 
 
+    const handleUpdateCard = (e, _id) => {
+        e.preventDefault();
+        const form = e.target;
+        const thumbnail = form.thumbnail.value;
+        const post_title = form.post_title.value;
+        const description = form.description.value;
+        const category = form.category.value;
+        const location = form.location.value;
+        const volunteers_needed = form.volunteers_needed.value;
+        const deadline = form.deadline.value;
+        const organizer_name = form.organizer_name.value;
+        const organizer_email = form.organizer_email.value;
+
+        const updatePost = { thumbnail, post_title, description, category, location, volunteers_needed, deadline, organizer_name, organizer_email }
+
+        fetch(`http://localhost:5000/post/${_id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatePost)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    form.reset()
+                    toast.success('Your volunteer post is updated now!')
+                }
+            })
+
+    }
+
+
     return (
         <div>
             <div className="overflow-x-auto">
@@ -75,10 +116,120 @@ const VolunteerVacancy = () => {
                                                 <td>{post.category}</td>
                                                 <td>{post.location}</td>
                                                 <td className="px-2 py-4">
-                                                    <Link className="text-white bg-green-500 p-2  rounded-xl hover:bg-blue-300 mr-2">Update</Link>
+                                                    {/* <Link className="text-white bg-green-500 p-2  rounded-xl hover:bg-blue-300 mr-2">Update</Link> */}
+                                                    {/* modal  */}
+                                                    {/* <div className="flex justify-center items-center py-6"> */}
+                                                    <button className="text-white bg-green-500 p-2 rounded-xl hover:bg-green-300 mr-2" onClick={() => document.getElementById('my_modal_5').showModal()}>Update</button>
+                                                    <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                                                        <div className="modal-box">
+                                                            <div className="">
+
+                                                                <div className="p-2 ">
+                                                                    <h2 className="text-xl font-bold my-4">Be A Volunteer</h2>
+                                                                    <form onSubmit={(e) => handleUpdateCard(e, post._id)}>
+                                                                        {/* form  row */}
+                                                                        <div className="md:flex mb-8 gap-2">
+                                                                            <div className="form-control md:w-1/2">
+                                                                                <label className="label">
+                                                                                    <span className="label-text font-medium text-lg text-[#808000]">Thumbnail</span>
+                                                                                </label>
+                                                                                <label className="input-group">
+                                                                                    <input type="text" name="thumbnail" defaultValue={post.thumbnail} placeholder="Thumbnail" className="input input-bordered w-full" />
+                                                                                </label>
+                                                                            </div>
+                                                                            <div className="form-control md:w-1/2 ">
+                                                                                <label className="label ">
+                                                                                    <span className="label-text font-medium text-lg text-[#808000]">Post title</span>
+                                                                                </label>
+                                                                                <label className="input-group">
+                                                                                    <input type="text" name="post_title" defaultValue={post.post_title} placeholder="Post title" className="input input-bordered w-full" />
+                                                                                </label>
+                                                                            </div>
+                                                                        </div>
+                                                                        {/* form row */}
+                                                                        <div className="md:flex mb-8 gap-2">
+                                                                            <div className="form-control md:w-1/2">
+                                                                                <label className="label">
+                                                                                    <span className="label-text font-medium text-lg text-[#808000]" >Description</span>
+                                                                                </label>
+                                                                                <label className="input-group">
+                                                                                    <input type="text" name="description" defaultValue={post.description} placeholder="Description" className="input input-bordered w-full" />
+                                                                                </label>
+                                                                            </div>
+                                                                            <div className="form-control md:w-1/2 ">
+                                                                                <label className="label">
+                                                                                    <span className="label-text font-medium text-lg text-[#808000]" >Category</span>
+                                                                                </label>
+                                                                                <label className="input-group">
+                                                                                    <input type="text" name="category" defaultValue={post.category} placeholder="Category" className="input input-bordered w-full" />
+                                                                                </label>
+                                                                            </div>
+                                                                        </div>
+                                                                        {/* form  row */}
+                                                                        <div className="md:flex mb-8 gap-2">
+                                                                            <div className="form-control md:w-1/2">
+                                                                                <label className="label">
+                                                                                    <span className="label-text font-medium text-lg text-[#808000]" >Location</span>
+                                                                                </label>
+                                                                                <label className="input-group">
+                                                                                    <input type="text" name="location" defaultValue={post.location} placeholder="Location" className="input input-bordered w-full" />
+                                                                                </label>
+                                                                            </div>
+                                                                            <div className="form-control md:w-1/2 ">
+                                                                                <label className="label">
+                                                                                    <span className="label-text font-medium text-lg text-[#808000]" >No of Volunteer</span>
+                                                                                </label>
+                                                                                <label className="input-group">
+                                                                                    <input type="number" name="volunteers_needed" defaultValue={post.volunteers_needed} placeholder="No of Volunteer" className="input input-bordered w-full" />
+                                                                                </label>
+                                                                            </div>
+                                                                        </div>
+                                                                        {/* form  row */}
+                                                                        <div className="mb-8 gap-2">
+                                                                            <div className="form-control w-full">
+                                                                                <label className="label">
+                                                                                    <span className="label-text font-medium text-lg text-[#808000]" >Deadline</span>
+                                                                                </label>
+                                                                                <label className="input-group">
+                                                                                    <DatePicker selected={post.deadline} onChange={(date) => setStartDate(date)} name="deadline" className="input input-bordered w-full" required />
+                                                                                    {/* <input type="text"  placeholder="deadline"  /> */}
+                                                                                </label>
+
+                                                                            </div>
+                                                                        </div>
+                                                                        {/* form row */}
+                                                                        <div className="md:flex mb-8 gap-2">
+                                                                            <div className="form-control md:w-1/2">
+                                                                                <label className="label">
+                                                                                    <span className="label-text font-medium text-lg text-[#808000]" >Organizer Name</span>
+                                                                                </label>
+                                                                                <label className="input-group">
+                                                                                    <input type="text" name="organizer_name" placeholder="Organizer Name" defaultValue={post.organizer_name} className="input input-bordered w-full" readOnly />
+                                                                                </label>
+                                                                            </div>
+                                                                            <div className="form-control md:w-1/2 ">
+                                                                                <label className="label">
+                                                                                    <span className="label-text font-medium text-lg text-[#808000]" >Organizer Email</span>
+                                                                                </label>
+                                                                                <label className="input-group">
+                                                                                    <input type="text" name="organizer_email" placeholder="Organizer Email" defaultValue={post.organizer_email} className="input input-bordered w-full" readOnly />
+                                                                                </label>
+                                                                            </div>
+                                                                        </div>
+                                                                        <input type="submit" value="Update Post" className="btn btn-block bg-[#808000] font-medium text-lg text-white" />
+                                                                    </form>
+                                                                </div>
+
+                                                                <form method="dialog">
+                                                                    <button className="btn border-[#808000] font-medium border-2 text-lg text-[#808000] w-full">Cancel</button>
+                                                                </form>
+
+                                                            </div>
+                                                        </div>
+                                                    </dialog >
+
                                                     <button onClick={() => handleDelete(post._id)} className="text-green-500 border-green-500 border-2 p-2 rounded-xl hover:bg-blue-200">Delete</button>
                                                 </td>
-
                                             </tr>
                                         ))}
                                     </tbody>
@@ -88,11 +239,13 @@ const VolunteerVacancy = () => {
                         (<div className=" h-[400px] w-full flex flex-col justify-center items-center gap-2">
                             <p className="text-2xl font-semibold">Your post Data is empty</p>
                             <p className="text-2xl font-semibold">Add your data <span className="text-green-500">now !</span> </p>
-                            <Link  className="btn text-white bg-green-500 ">Add Now !</Link>
+                            <Link className="btn text-white bg-green-500 ">Add Now !</Link>
                         </div>)
 
                 }
             </div>
+            <ToastContainer></ToastContainer>
+
         </div>
 
     );
